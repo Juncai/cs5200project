8b6b7ed0cb9b7803e5e88bc97aa9044582f8e93d
Jon <joncai2012@gmail.com>
2014-01-10 13:34:38 -0500

    add my name

--- try/src/neu/edu/JJ.java
+++ try/src/neu/edu/JJ.java
@@ -1,10 +1,10 @@
 package neu.edu;
 
-
 public class JJ {
 	
 	public static void main(String[] args) {
 		System.out.println("TengjiaoChen");
 		System.out.println("ZT C");
+		System.out.println("Jun Cai");
 	}
 }
