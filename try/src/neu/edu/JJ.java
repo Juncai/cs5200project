533f5f99d3a982f8c50945f4188753d0fc10982a
TengjiaoChen <ctjqintoko@gmail.com>
2014-01-10 16:38:30 -0500

    jj

--- try/src/neu/edu/JJ.java
+++ try/src/neu/edu/JJ.java
@@ -4,7 +4,7 @@ public class JJ {
 	
 	public static void main(String[] args) {
 		System.out.println("TengjiaoChen");
-		System.out.println("ZT C");
+		System.out.println("ZT C");  
 		System.out.println("Jun Cai");
 		System.out.println("Jun Cai");
 	}
